892f233297a7d4c16bfafd92f8c880da39d09c75
xelerrrrr <xeler136@gmail.com>
2024-09-14 11:04:50 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 function myFunction() {
     console.log("JavaScript is working!");
+
+    
 }
+
+    // <script async src="https://www.googletagmanager.com/gtag/js?id=G-4DJG6GNVE5"></script>
+    // <script>
+    //   window.dataLayer = window.dataLayer || [];
+    //   function gtag(){dataLayer.push(arguments);}
+    //   gtag('js', new Date());
+    
+    //   gtag('config', 'G-4DJG6GNVE5');
+    // </script>
 
 myFunction();
